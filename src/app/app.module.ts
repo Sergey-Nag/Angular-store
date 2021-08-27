d1767b1c56f96e0affe1d530a4db6996bfab921a
Serhii Nahornyi <snah@ciklum.com>
2021-08-27 15:05:09 +0300

    components setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { BooksCatalogComponent } from './components/books-catalog/books-catalog.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CatalogComponent,
+    ProductDetailComponent,
+    BooksCatalogComponent,
+    CartComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/books-catalog/books-catalog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-books-catalog',
+  templateUrl: './books-catalog.component.html',
+  styleUrls: ['./books-catalog.component.scss']
+})
+export class BooksCatalogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
