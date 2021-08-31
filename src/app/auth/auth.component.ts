a20a24fb4fd99e3e8a43c446d1fe0b347c3d2ff6
Serhii Nahornyi <snah@ciklum.com>
2021-08-31 13:26:17 +0300

    auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,22 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AuthService } from './auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnDestroy, OnInit {
-
-  constructor(private authService: AuthService) {
-  }
-
-  ngOnInit() {
-    this.authService.setAuth(false);
-  }
-
-  ngOnDestroy() {
-    this.authService.setAuth(true);
-  }
-
+export class AuthComponent {
 }
