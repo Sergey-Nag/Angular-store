17b5a73e526293b1b7518dda481f9f41920450a2
Serhii Nahornyi <snah@ciklum.com>
2021-09-01 17:59:34 +0300

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/modules/auth/models/user.model';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  userdata!: {username: string, avatar: string};
+  userSubscription!: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.userSubscription = this.authService.user.subscribe((user: User | null) => {
+      if (!user) return;
+
+      const { username, avatar } = user;
+      this.userdata = { username, avatar };
+    });
+  }
+
+  onSignOut() {
+    this.authService.signOut();
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 
 }
