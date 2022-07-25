8b09bdb4fc1537edbdc99eec6ef0db1c63fc85f4
Serhii Nahornyi <snah@ciklum.com>
2022-07-25 10:03:19 +0300

    Removed timer after auth

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { BehaviorSubject, interval } from 'rxjs';
+import { BehaviorSubject, interval, Subscription } from 'rxjs';
 import { finalize, take, tap } from 'rxjs/operators';
 import { User } from '../../models/user.model';
 import { AuthService } from '../../services/auth.service';
@@ -12,9 +12,7 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  avatarUrl = '../../../assets/unknown-guy.svg';
   isLoading = false;
-  loadedData = new BehaviorSubject<{name: string, time: number}>(null);
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -25,32 +23,11 @@ export class LoginComponent {
 
     this.authService
       .signIn(form.value.username)
-      .subscribe((data: User) => {
-        this.avatarUrl = data.avatar;
-
-        this.loadedData.next({
-          name: data.username,
-          time: 5
-        });
-
-        interval(1000)
-          .pipe(
-            take(5),
-            tap((x) => {
-              this.loadedData.next({
-                name: data.username,
-                time: 5 - (x + 1)
-              });
-            }),
-            finalize(()=> {
-              this.router.navigate(['/']);
-              this.authService.setAuth(true);
-            })
-          )
-          .subscribe()
-
+      .pipe(take(1))
+      .subscribe(() => {
+        this.authService.setAuth(true);
         this.isLoading = false;
+        this.router.navigate(['/']);
       });
   }
-
 }
